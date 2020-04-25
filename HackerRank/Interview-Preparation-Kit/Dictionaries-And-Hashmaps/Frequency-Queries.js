86c0b9a31c7fb7865071fdec72e6d44db4980cc7
Nicklas766 <Nicklas766@live.se>
2020-04-25 18:47:20 +0200

    new solution added

--- HackerRank/Interview-Preparation-Kit/Dictionaries-And-Hashmaps/Frequency-Queries.js
+++ HackerRank/Interview-Preparation-Kit/Dictionaries-And-Hashmaps/Frequency-Queries.js
@@ -1,5 +1,54 @@
+/** 
+ * Frequency Queries
+ */
 
+// Spread operator seems not to work to do due to time complexity so push is requried.
+function freqQuery(queries) {
+    const 
+    numOfElements = {},
+    pairs = {};
 
+    const addToPair = (pairAmount) => {
+        if (pairs[pairAmount]) {
+            pairs[pairAmount].push(true);
+        } else {
+            pairs[pairAmount] = [true];
+        }
+    }
+
+    
+    return queries.reduce((acc, curr) => {
+
+        const [query, x] = curr;
+
+        if (query === 1) {
+            if (numOfElements[x]) {
+                pairs[numOfElements[x]].pop();
+                numOfElements[x] += 1;
+            } else {
+                numOfElements[x] = 1;
+            }
+            addToPair(numOfElements[x]);
+        }
+
+        if (query === 2 && numOfElements[x]) {
+            pairs[numOfElements[x]].pop();
+            numOfElements[x] -= 1;
+            addToPair(numOfElements[x]);
+        }
+
+        if (query === 3) {
+            if (pairs[x] && pairs[x].length) {
+                acc.push(1);
+                return acc
+            }
+            acc.push(0);
+            return acc
+        }
+        
+        return acc;
+    }, []);
+}
 
 
 // I prefer this even though it's slower:
