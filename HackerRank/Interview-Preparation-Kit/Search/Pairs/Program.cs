8fd9b99c02a375a9187b70f6f43f2379fc88363c
nicklas766 <nicklas766@live.se>
2021-04-11 12:23:44 +0200

    pairs solution in c#

---
+++ HackerRank/Interview-Preparation-Kit/Search/Pairs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Pairs
+
+namespace Pairs
+{
+    class Program
+    {
+        // Complexity O(N)
+        static int pairs(int k, int[] arr) 
+        {
+            int foundPairs = 0;
+            HashSet<int> integers = new HashSet<int>();
+            
+            foreach (int num in arr)
+            {
+                if (integers.Contains(num - k))
+                {
+                    foundPairs++;
+                }
+                if (integers.Contains(num + k))
+                {
+                    foundPairs++;
+                }
+                integers.Add(num);
+            }
+            return foundPairs;
+        }
+
+        static void Main(string[] args) 
+        {
+            Console.WriteLine(pairs(1, new int[] { 1, 2, 3, 4 }));
+            Console.WriteLine(pairs(2, new int[] { 1, 5, 3, 4, 2 }));
+            Console.WriteLine(pairs(2, new int[] { 1, 3, 5, 8, 6, 4, 2 }));
+        }
+    }
+}
